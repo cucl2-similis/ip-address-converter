bd8607748d4f53d3aa2b5de50f10c879516ed971
cucl2-similis <78360973+cucl2-similis@users.noreply.github.com>
2024-03-27 19:19:32 +0900

    [v1.0.0]Home初期化 #1
    ・イベントハンドラ対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 export default function Home() {
   return (
     <div className="container">
       <h2>Home</h2>
+      <button type="button" className="btn btn-primary" onClick={() => alert("OK")}>Confirm</button>
     </div>
   );
 }
