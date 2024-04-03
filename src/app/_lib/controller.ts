425e474f12cfd09ea99cdd6b6d508116f05a9286
cucl2-similis <78360973+cucl2-similis@users.noreply.github.com>
2024-04-04 06:14:45 +0900

    [v1.0.0]入力フォーム作成 #3
    ・入力値変更中の再検証機能追加

--- src/app/_lib/controller.ts
+++ src/app/_lib/controller.ts
@@ -3,6 +3,7 @@ import { Assertions } from "./assertions";
 import { Regex } from "./const";
 import { ResultDto } from "./result-dto";
 import { Validator } from "./validator";
+import { View } from "./view";
 
 /**
  * コントローラ
@@ -12,10 +13,9 @@ export class Controller {
     /** バリデータ */
     private readonly validator: Validator;
 
-    /** `was-validated`クラス設定要否boolean用 stateセッタ関数 */
-    private readonly setWasValidated: Dispatch<SetStateAction<boolean>>;
-    /** `invalid-feedback`クラス要素内容文字列用 stateセッタ関数 */
-    private readonly setInvalidFeedback: Dispatch<SetStateAction<string>>;
+    /** ビュー（画面表示制御） */
+    private readonly view: View;
+
     /** 変換結果DTO用 stateセッタ関数 */
     private readonly setResultDto: Dispatch<SetStateAction<ResultDto | null>>;
 
@@ -28,11 +28,8 @@ export class Controller {
     public constructor(setWasValidated: Dispatch<SetStateAction<boolean>>,
                        setInvalidFeedback: Dispatch<SetStateAction<string>>,
                        setResultDto: Dispatch<SetStateAction<ResultDto | null>>) {
-
         this.validator = new Validator();
-
-        this.setWasValidated = setWasValidated;
-        this.setInvalidFeedback = setInvalidFeedback;
+        this.view = new View(setWasValidated, setInvalidFeedback);
         this.setResultDto = setResultDto;
     }
 
@@ -48,11 +45,23 @@ export class Controller {
 
         this.validator.validate(formElement, inputElement);
 
+        // 入力チェックエラーありの場合
         if (this.validator.hasErrors()) {
-            this.setWasValidated(true);
-            this.setInvalidFeedback(inputElement.validationMessage);
+
+            // 入力値の変更イベント発生時の関数を設定
+            inputElement.oninput = () => {
+                this.validator.validate(formElement, inputElement);           // 再検証
+                this.view.updateErrorMessage(inputElement.validationMessage); // 画面表示内容更新
+            };
+
+            // バリデータによる検証で<input>要素に設定されたエラーメッセージで画面表示内容を更新
+            this.view.updateErrorMessage(inputElement.validationMessage);
             return;
         }
+
+        // 入力チェックエラーなしの場合
+        inputElement.oninput = () => {}; // 入力値の変更イベント発生時の関数を削除
+        this.view.updateErrorMessage();  // エラーメッセージを非表示に更新
 
         const decIpAddressArray = inputElement.value              // IP:     1.2.3.4/5
                                     .split(Regex.PERIOD_OR_SLASH) // array = 1,2,3,4,5 ──┐
