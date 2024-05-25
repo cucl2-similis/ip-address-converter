4e331836aa108c441fcd90f545eb3bbcb3baf06a
cucl2-similis <78360973+cucl2-similis@users.noreply.github.com>
2024-05-25 12:26:31 +0900

    [v1.1.0]入力フォーム形式変更 #13
    ・コントローラ引数名修正
    ・リンク（リポジトリ／サイト）追加

--- src/app/_lib/controller.ts
+++ src/app/_lib/controller.ts
@@ -34,43 +34,43 @@ export class Controller {
     /**
      * 入力IPアドレス変換
      * @param formElement `<form>`要素
-     * @param inputIpv4 IPv4`<input>`要素
-     * @param inputCidr CIDR`<input>`要素
+     * @param ipv4InputElement IPv4アドレス`<input>`要素
+     * @param cidrInputElement CIDRブロック`<input>`要素
      */
     public convert(formElement: HTMLFormElement | null,
-                   inputIpv4: HTMLInputElement | null,
-                   inputCidr: HTMLInputElement | null): void {
+                   ipv4InputElement: HTMLInputElement | null,
+                   cidrInputElement: HTMLInputElement | null): void {
 
         Assertions.assertNotNull(formElement);
-        Assertions.assertNotNull(inputIpv4);
-        Assertions.assertNotNull(inputCidr);
+        Assertions.assertNotNull(ipv4InputElement);
+        Assertions.assertNotNull(cidrInputElement);
 
         // 入力チェック
-        this.validator.validate(formElement, inputIpv4, inputCidr);
+        this.validator.validate(formElement, ipv4InputElement, cidrInputElement);
 
         // 入力チェックエラーありの場合
         if (this.validator.hasErrors()) {
 
             // 入力値の変更イベント発生時の関数を設定
             const onInputFunction = () => {
-                this.validator.validate(formElement, inputIpv4, inputCidr);                              // 再検証
-                this.view.updateErrorMessages(inputIpv4.validationMessage, inputCidr.validationMessage); // 画面表示内容更新
+                this.validator.validate(formElement, ipv4InputElement, cidrInputElement);                              // 再検証
+                this.view.updateErrorMessages(ipv4InputElement.validationMessage, cidrInputElement.validationMessage); // 画面表示内容更新
             };
-            inputIpv4.oninput = onInputFunction; // IPv4アドレス<input>要素に設定
-            inputCidr.oninput = onInputFunction; // CIDRブロック<input>要素に設定
+            ipv4InputElement.oninput = onInputFunction; // IPv4アドレス<input>要素に設定
+            cidrInputElement.oninput = onInputFunction; // CIDRブロック<input>要素に設定
 
             // バリデータの検証によって<input>要素に設定されたエラーメッセージで画面表示内容を更新
-            this.view.updateErrorMessages(inputIpv4.validationMessage, inputCidr.validationMessage);
+            this.view.updateErrorMessages(ipv4InputElement.validationMessage, cidrInputElement.validationMessage);
             return;
         }
 
         // 入力チェックエラーなしの場合
-        inputIpv4.oninput = () => {};   // 入力値の変更イベント発生時の関数を削除（IPv4アドレス<input>要素）
-        inputCidr.oninput = () => {};   // 入力値の変更イベント発生時の関数を削除（CIDRブロック<input>要素）
-        this.view.updateErrorMessage(); // エラーメッセージを非表示に更新
+        ipv4InputElement.oninput = () => {}; // 入力値の変更イベント発生時の関数を削除（IPv4アドレス<input>要素）
+        cidrInputElement.oninput = () => {}; // 入力値の変更イベント発生時の関数を削除（CIDRブロック<input>要素）
+        this.view.updateErrorMessage();      // エラーメッセージを非表示に更新
 
         // 変換
-        const resultDto = this.converter.convert(inputIpv4.value, inputCidr.value); // 変換処理実行
-        this.view.updateResult(resultDto);                                          // 変換結果更新
+        const resultDto = this.converter.convert(ipv4InputElement.value, cidrInputElement.value); // 変換処理実行
+        this.view.updateResult(resultDto);                                                        // 変換結果更新
     }
 }
