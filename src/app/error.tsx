265d4b66dd2df74d51068177cff1ae2081e7238a
cucl2-similis <78360973+cucl2-similis@users.noreply.github.com>
2024-11-29 20:11:57 +0900

    [v1.2.0]エラーハンドリング対応 #19
    ・Errorコンポーネント追加
    ・テストケース追加（Error）

---
+++ src/app/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function Error({
+  error,
+  reset
+}: Readonly<{
+  error: Error;
+  reset: () => void;
+}>) {
+  return (
+    <div className="container">
+      <div className="px-4 py-5 my-5 text-center">
+        <i className="bi bi-exclamation-triangle text-warning display-3" />
+        <h1 className="mb-4">Something went wrong.</h1>
+        <div className="d-grid gap-3 d-sm-flex justify-content-sm-center">
+          <button type="button" className="btn btn-primary" onClick={reset}>Retry</button>
+          <button type="button" className="btn btn-outline-primary" onClick={() => location.reload()}>Reload</button>
+        </div>
+      </div>
+    </div>
+  );
+}
